18f780b66b91606d82555072491d8298e9d9ea61
Aley133 <103179994+Aley133@users.noreply.github.com>
2025-08-14 18:34:57 +0500

    Add files via upload

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -1,1 +1,34 @@
+from logging.config import fileConfig
+from sqlalchemy import engine_from_config, pool
+from alembic import context
+import os
 
+config = context.config
+if config.config_file_name is not None:
+    fileConfig(config.config_file_name)
+
+DB_URL = os.getenv("DATABASE_URL", "")
+config.set_main_option("sqlalchemy.url", DB_URL)
+
+target_metadata = None
+
+def run_migrations_offline():
+    context.configure(url=DB_URL, literal_binds=True, dialect_opts={"paramstyle": "named"})
+    with context.begin_transaction():
+        context.run_migrations()
+
+def run_migrations_online():
+    connectable = engine_from_config(
+        config.get_section(config.config_ini_section),
+        prefix="sqlalchemy.",
+        poolclass=pool.NullPool,
+    )
+    with connectable.connect() as connection:
+        context.configure(connection=connection)
+        with context.begin_transaction():
+            context.run_migrations()
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
